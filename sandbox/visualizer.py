151fa3fa939b9e8b2f779c130532a924c0ac3a6f
Shreyas <sharora@gmail.com>
2021-03-08 13:49:25 -0800

    particle localization with improved graphics, pf SLAM works but there is some drift, needs further investigation

--- sandbox/visualizer.py
+++ sandbox/visualizer.py
@@ -5,7 +5,9 @@ from multiprocessing.connection import Listener
 #conversion factor
 pix2inch = 4
 #field with in inches
-fieldwidth = 144
+fieldwidth = 288
+#occupancy grid to inch
+oc2inch = 4
 
 #robot size in inches
 robotsize = 18
@@ -13,8 +15,21 @@ robotsize = 18
 #window, batch, robot definition
 sim_window = pyglet.window.Window(pix2inch*fieldwidth, pix2inch*fieldwidth)
 batch = graphics.Batch()
-robot = shapes.Rectangle(-100, -100, robotsize * pix2inch, robotsize * pix2inch, (255, 255, 255), batch=batch)
 
+
+#creating occupancy grid
+ocgraphic = []
+oclen = int(fieldwidth/oc2inch)
+ocwidth = int(fieldwidth/oc2inch)
+cell2pixel = oc2inch * pix2inch
+
+for i in range(oclen):
+    ocgraphic.append([])
+    for j in range(ocwidth):
+        ocgraphic[i].append(shapes.Rectangle(cell2pixel*j, cell2pixel*i, cell2pixel-1, cell2pixel - 1, (0,0,0), batch=batch))
+
+
+robot = shapes.Rectangle(-100, -100, robotsize * pix2inch, robotsize * pix2inch, (255, 255, 255), batch=batch)
 
 # debug = []
 # debug.append(shapes.Line(42*4,4*114, 4*128, 4*6,1,color=(255,0,0), batch=batch))
@@ -25,7 +40,7 @@ mapsquares = []
 lidarscan = []
 
 #setting center of rotation to be center of the robot
-robot._anchor_x = pix2inch*robotsize/2 
+robot._anchor_x = pix2inch*robotsize/2
 robot._anchor_y = pix2inch*robotsize/2
 
 
@@ -51,7 +66,7 @@ def update_pos(dt):
             if(len(points) == len(msg[1])):
                 points[i].x = pix2inch*temp._x[0]
                 points[i].y = pix2inch*temp._x[1]
-                points[i].radius = pix2inch*temp._w*20
+                points[i].radius = max(1,pix2inch*temp._w*20)
             else:
                 points.append(shapes.Circle(pix2inch*temp._x[0], pix2inch*temp._x[1],pix2inch*3,color=(255,0,0), batch=batch))
     elif(msg[0] == "map"):
@@ -62,28 +77,32 @@ def update_pos(dt):
         for i in range(length):
             for j in range(width):
                 if(oc._oc[length - i - 1][j] == 0):
-                    mapsquares.append(shapes.Rectangle(cell2pix*j, cell2pix*i, cell2pix-1, cell2pix - 1, (115,3,252), batch=batch))
+                    ocgraphic[i][j].color = (115,3,252)
     elif(msg[0] == "lidar"):
         ls = msg[1]
         xt = msg[2]
-        # length = ls.shape[0]
-        # width = ls.shape[1]
-        #TODO remove hardcode
-        cell2pix = 4*pix2inch
+        for i in range(oclen):
+            for j in range(ocwidth):
+                if(ocgraphic[i][j].color == [0,255,0]):
+                    ocgraphic[i][j].color = (0,0,0)
+                elif(ocgraphic[i][j].color == [255,0,0]):
+                    ocgraphic[i][j].color = (115,3,252)
         for i in range(len(ls)):
             for j in range(len(ls[i])):
                 xls = ls[i][j][0] + int(xt[0]/4)
                 yls = ls[i][j][1] + int(xt[1]/4)
                 if(j == len(ls[i]) - 1):
-                    lidarscan.append(shapes.Rectangle(cell2pix*xls, cell2pix*yls, cell2pix-1, cell2pix - 1, (255,0,0), batch=batch))
+                    ocgraphic[yls][xls].color = (255,0,0)
                 else:
-                    lidarscan.append(shapes.Rectangle(cell2pix*xls, cell2pix*yls, cell2pix-1, cell2pix - 1, (0,255,0), batch=batch))
-
+                    ocgraphic[yls][xls].color = (0,255,0)
+    elif(msg[0] == "clear"):
+        for i in range(len(ocgraphic)):
+            for j in range(len(ocgraphic[0])):
+                    ocgraphic[i][j].color = (0,0,0)
     else:
         robot.x = msg[0]*pix2inch
         robot.y = msg[1]*pix2inch
         robot.rotation = msg[2]
-
 
 if __name__ == '__main__':
     pyglet.clock.schedule_interval(update_pos, 1/50.0)
